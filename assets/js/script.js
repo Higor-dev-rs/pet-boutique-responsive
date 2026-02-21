8373049df687cb46c05c7d14b472ddce4561843a
Higor <higorrodrigues1605@gmail.com>
2026-02-20 23:02:17 -0300

    feat: pequenas melhorias e correção de bug no footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,11 @@ const btnTopo = document.querySelector("#btn-topo");
 
 // para mostrar o botão quando descer pelo menos 200px
 window.addEventListener("scroll", function() {
-    if (window.innerWidth > 768) {
         if (window.scrollY > 200) {
             btnTopo.classList.add("show");
         } else {
             btnTopo.classList.remove("show");
         }
-
-    }
 });
 
 
